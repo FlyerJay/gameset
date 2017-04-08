96ffa3ef351fd5c5fc888b141fd7dfdc268717c8
flyerjay <876173839@qq.com>
2017-04-08 11:18:28 +0800

    更新游戏代码

--- snaker/js/index.js
+++ snaker/js/index.js
@@ -13,7 +13,7 @@ canvas.style.marginLeft="-240px";
 document.body.appendChild(canvas);
 
 var snaker = {
-    speed:100,
+    speed:70,
     body:[[2,0],[1,0],[0,0]],
     direction:3,//0上,1下,2左,3右
 }
@@ -23,7 +23,6 @@ var keyDown = {};
 var foodPosition = {
     x:-1,
     y:-1,
-    speed:1,
 };
 var isEat = false;
 
@@ -48,52 +47,37 @@ addEventListener('keydown',function(e){
     }
 });
 
-var init = function(){
-    //先画横线
-    ctx.strokeStyle = '#666';
-    ctx.lineWidth = 1;
-    for(var i = 0; i <= 24; i++){
-        ctx.moveTo(0,i*20);
-        ctx.lineTo(480,i*20);
-    }
-    //再画竖线
-    for(var i = 0; i <= 24; i++){
-        ctx.moveTo(i*20,0);
-        ctx.lineTo(i*20,480);
-    }
-    ctx.stroke();
-    ctx.save();
-}
-
 function drawSnaker(){
     ctx.clearRect(0,0,480,480);
-    //init();
     drawFood();
-    var j = 0;
+    var j = 1;
+    ctx.fillStyle = '#9ea7b4'
     while(j < snaker.body.length){
         ctx.fillRect(snaker.body[j][0]*20+1,snaker.body[j][1]*20+1,19,19)
         j++;
     }
+    ctx.save();
+    ctx.fillStyle = '#657180';
+    ctx.fillRect(snaker.body[0][0]*20+1,snaker.body[0][1]*20+1,19,19)
+    ctx.restore();
 }
-
 
 function productFood(){
     var x = Math.floor((Math.random()*24))
     var y = Math.floor((Math.random()*24))
-    for(arr in snaker.body){
-        if(x==arr[0] && y==arr[1]){
+    for(var i = 0; i < snaker.body.length; i++){
+        if(x == snaker.body[i][0] && y == snaker.body[i][1]){
             productFood();
-            return;
-        } 
+            return false;
+        }
     }
     foodPosition.x = x;
     foodPosition.y = y; 
-    console.log(foodPosition.x,foodPosition.y)
 }
 
 function drawFood(){
     ctx.save();
-    ctx.fillStyle = 'green';
+    ctx.fillStyle = '#00cc66';
     ctx.fillRect(foodPosition.x*20+1,foodPosition.y*20+1,19,19)
     ctx.restore();
 }
@@ -110,63 +94,34 @@ function snakerMove(){
         isEat = true;
     }
     drawSnaker();
-    window.requestAnimationFrame(snakerMove)
+    window.requestAnimationFrame(snakerMove);
 }
 
 function main(){
-    //init()
-    drawSnaker();
-    drawFood();
     productFood();
     snakerMove();
     setInterval(function(){
+        var i = snaker.body.length - 1;
+        while(i > 0){
+            snaker.body[i][0] = snaker.body[i-1][0];
+            snaker.body[i][1] = snaker.body[i-1][1];
+            i--;
+        }
         switch(snaker.direction){
             case 0:
-                var i = snaker.body.length - 1;
-                var copy = snaker.body;
-                while(i > 0){
-                    copy[i][0] = snaker.body[i-1][0];
-                    copy[i][1] = snaker.body[i-1][1];
-                    i--;
-                }
-                copy[0][1] > 0 ? copy[0][1]-- : copy[0][1] = 23;
-                snaker.body = copy;
+                snaker.body[0][1] > 0 ? snaker.body[0][1]-- : snaker.body[0][1] = 23;
                 break;
             case 1:
-                var i = snaker.body.length - 1;
-                var copy = snaker.body;
-                while(i > 0){
-                    copy[i][0] = snaker.body[i-1][0];
-                    copy[i][1] = snaker.body[i-1][1];
-                    i--;
-                }
-                copy[0][1] < 23 ? copy[0][1]++ : copy[0][1] = 0;
-                snaker.body = copy;
+                snaker.body[0][1] < 23 ? snaker.body[0][1]++ : snaker.body[0][1] = 0;
                 break;
             case 2:
-                var i = snaker.body.length - 1;
-                var copy = snaker.body;
-                while(i > 0){
-                    copy[i][0] = snaker.body[i-1][0];
-                    copy[i][1] = snaker.body[i-1][1];
-                    i--;
-                }
-                copy[0][0] > 0 ? copy[0][0]-- : copy[0][0] = 23;
-                snaker.body = copy;
+                snaker.body[0][0] > 0 ? snaker.body[0][0]-- : snaker.body[0][0] = 23;
                 break;
             case 3:
-                var i = snaker.body.length - 1;
-                var copy = snaker.body;
-                while(i > 0){
-                    copy[i][0] = snaker.body[i-1][0];
-                    copy[i][1] = snaker.body[i-1][1];
-                    i--;
-                }
-                copy[0][0] < 23 ? copy[0][0]++ : copy[0][0] = 0;
-                snaker.body = copy;
+                snaker.body[0][0] < 23 ? snaker.body[0][0]++ : snaker.body[0][0] = 0;
                 break;
         }
-    },(1000/snaker.speed*10));
+    },100-snaker.speed);
 }
 
 main();
