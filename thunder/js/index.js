15214d13b77549bcf1911871aa2a11eafac62e4f
flyer <876173839@qq.com>
2017-04-10 22:03:52 +0800

    更新代码

--- thunder/js/index.js
+++ thunder/js/index.js
@@ -11,13 +11,6 @@ canvas.style.marginLeft = '-200px';
 canvas.style.marginTop = '-300px';
 document.body.appendChild(canvas);
 
-var scene = document.querySelectorAll('.background-bottom')[0];
-var scene1 = document.querySelectorAll('.background-bottom .background')[0];
-var scene2 = document.querySelectorAll('.background-bottom .background')[1];
-var initColor = 250;
-var INTER = 25;
-var DOUBLEINTER = 50;
-var backgroundSEC = true;
 var boomImage = new Image();
 boomImage.src = './images/boom.png';
 var boomReady = false;
@@ -25,34 +18,25 @@ boomImage.onload = function(){
     boomReady = true;
 };
 
-scene2.style.backgroundImage = `-webkit-gradient(linear,center top,center bottom,from(rgb(${initColor-INTER},${initColor-INTER},${initColor-INTER})), to(rgb(${initColor},${initColor},${initColor})))`;
-scene1.style.backgroundImage = `-webkit-gradient(linear,center top,center bottom,from(rgb(${initColor-DOUBLEINTER},${initColor-DOUBLEINTER},${initColor-DOUBLEINTER})), to(rgb(${initColor-INTER},${initColor-INTER},${initColor-INTER})))`;
+var heroImage = new Image();
+heroImage.src = './images/hero.png';
+var heroReady = false;
+heroImage.onload = function(){
+    heroReady = true;
+}
 
-function background(){//背景移动
-    var bottom = scene.style.bottom.replace('px','') - 0;
-    if(bottom <= -600){
-        bottom = 0;
-        if(backgroundSEC){
-            initColor -= INTER;
-            scene2.style.backgroundImage = `-webkit-gradient(linear,center top,center bottom,from(rgb(${initColor-INTER},${initColor-INTER},${initColor-INTER})), to(rgb(${initColor},${initColor},${initColor})))`;
-            scene1.style.backgroundImage = `-webkit-gradient(linear,center top,center bottom,from(rgb(${initColor-DOUBLEINTER},${initColor-DOUBLEINTER},${initColor-DOUBLEINTER})), to(rgb(${initColor-INTER},${initColor-INTER},${initColor-INTER})))`;
-            if(initColor < 100){
-                backgroundSEC = false;
-                initColor -= INTER;
-            }
-        }else{
-            
-            scene1.style.backgroundImage = `-webkit-gradient(linear,center bottom,center top,from(rgb(${initColor-INTER},${initColor-INTER},${initColor-INTER})), to(rgb(${initColor},${initColor},${initColor})))`;
-            scene2.style.backgroundImage = `-webkit-gradient(linear,center bottom,center top,from(rgb(${initColor-DOUBLEINTER},${initColor-DOUBLEINTER},${initColor-DOUBLEINTER})), to(rgb(${initColor-INTER},${initColor-INTER},${initColor-INTER})))`;
-            initColor += INTER;
-            if(initColor >200){
-                backgroundSEC = true;
-                initColor += INTER;
-            }
-        }
-    }
-    scene.style.bottom = bottom - 2 + 'px';
-    requestAnimationFrame(background);
+var backgroundImage = new Image();
+backgroundImage.src = './images/map.jpg'
+var backgroundReady = false;
+backgroundImage.onload = function(){
+    backgroundReady = true;
+}
+
+var hulletImage = new Image();
+hulletImage.src = './images/hullet.png';
+var hulletReady = false;
+hulletImage.onload = function(){
+    hulletReady = true;
 }
 
 function Boom(){
@@ -65,38 +49,125 @@ function Boom(){
     }
     boom.prototype.startBoom = function(x,y,size){
         var self = this;
-        var boom = setInterval(function(){
-            if(boomReady && !self.boomEnd){
-                ctx.clearRect(x,y,size,size);
-                ctx.drawImage(boomImage,8.3+self.boomINTER*self.boomState,0,self.boomSize,self.boomSize,x,y,size,size);
-                self.boomState++;
-                if(self.boomState > 12){
-                    self.boomEnd = true;
-                    clearInterval(boom);
-                    ctx.clearRect(x,y,size,size);
-                    return false;
-                }
-            } 
-        },30);
+        if(boomReady && !self.boomEnd){
+            ctx.drawImage(boomImage,8.3+self.boomINTER*self.boomState,0,self.boomSize,self.boomSize,x,y,size,size);
+            self.boomState++;
+            if(self.boomState > 12){
+                self.boomEnd = true;
+                return false;
+            }
+        }
     }
     return new boom();
+}
+var hero = new Hero();
+
+addEventListener('keydown',function(e){
+    switch(e.keyCode){
+        case 38:
+            hero.direction.up = true;
+            break;
+        case 40:
+            hero.direction.down = true;
+            break;
+        case 37:
+            hero.direction.left = true;
+            break;
+        case 39:
+            hero.direction.right = true;
+            break;
+    }
+})
+
+addEventListener('keyup',function(e){
+    switch(e.keyCode){
+        case 38:
+            hero.direction.up = false;
+            break;
+        case 40:
+            hero.direction.down = false;
+            break;
+        case 37:
+            hero.direction.left = false;
+            break;
+        case 39:
+            hero.direction.right = false;
+            break;
+    }
+})
+
+function Hero(){
+    this.x = 160;
+    this.y = 540;
+    this.direction = {
+        up:false,
+        down:false,
+        left:false,
+        right:false,
+    };
+}
+
+var hullets = [];
+
+function Hullet(x,y){
+    this.x = x;
+    this.y = y;
 }
 
 function draw(){
     ctx.save();
     ctx.fillStyle = 'transparent';
     ctx.restore();
+    drawBg();
+    //Boom().startBoom(20,20,40);
+    drawHullet();
+    drawHero();
     requestAnimationFrame(draw);
 }
 
+function drawHero(){
+    if(hero.direction.up && hero.y > -50)
+        hero.y-=5;
+    if(hero.direction.down && hero.y < 550)
+        hero.y+=5;
+    if(hero.direction.left && hero.x > -50)
+        hero.x-=5;
+    if(hero.direction.right && hero.x < 350)
+        hero.x+=5;
+    ctx.drawImage(heroImage,hero.x,hero.y,100,100);
+}
+
+var by = -2048+600;
+
+function drawBg(){
+    by+=2;
+    if(by == -424) by = -2048+600;
+    ctx.drawImage(backgroundImage,0,by,400,1024);
+    ctx.drawImage(backgroundImage,0,by+1024,400,1024);
+}
+
+function shot(){
+    setInterval(function(){
+        hullets.push(new Hullet(hero.x,hero.y));
+    },200)
+}
+
+function drawHullet(){
+    for(var i = 0;i < hullets.length;i++){
+        ctx.drawImage(hulletImage,hullets[i].x+20,hullets[i].y-=10,60,60)
+    }
+    var copy = [];
+    for(var i = 0;i < hullets.length;i++){
+        if(hullets[i].y > -50){
+            copy.push(hullets[i])
+        }
+    }
+    hullets = copy;
+}
+
 function main(){
-    background();//绘制背景
     draw();
+    shot();
 }
 
 main();
-// setInterval(function(){
-//     var x = Math.floor(Math.random() * 340);
-//     var y = Math.floor(Math.random() * 540);
-//     Boom().startBoom(x,y,100);
-// },500)
